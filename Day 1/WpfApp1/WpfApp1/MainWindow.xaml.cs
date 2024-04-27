9a0c3873761d5a365327d6066df1db4a36fe9548
ScarlAsh <eseaboshanb@gmail.com>
2024-04-27 14:19:30 +0300

    uploading task

---
+++ Day 1/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //btn1.Content = "Click";
+            InitializeComponent();
+        }
+
+        private void btn1Clicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Btn1 is Clicked");
+
+        }
+    }
+}
---
+++ Labs/Day 3/new wpf app/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_wpf_app
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Image { get; set; }
+
+		public override string ToString()
+		{
+            return Name; 
+		}
+	}
+}
